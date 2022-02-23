1429c8c54d61d73486a95644451c6fd46d381982
noormohammad20 <mdsaim204@gmail.com>
2022-02-23 20:00:10 +0600

    Module Finished

---
+++ js/countries.js
@@ -0,0 +1,37 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+loadCountries()
+const displayCountries = counties => {
+
+    const countriesContainer = document.getElementById('countries')
+    counties.forEach(country => {
+        const div = document.createElement('div')
+        div.classList.add('country')
+        div.innerHTML = `
+        <h3>${country.name.common}<h3/>
+        <p>${country.capital}<p/>
+        <button onclick="loadCountryByName('${country.name.common}')"> Details </button>
+        `
+        countriesContainer.appendChild(div)
+    })
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    const countryDetail = document.getElementById('country-detail')
+    countryDetail.innerHTML = `
+    <h4>${country.name.common}</h4>
+    <p> ${country.population}</p>
+    <img width="200px" src="${country.flags.png}">
+    `
+
+}
---
+++ js/mealdb.js
@@ -0,0 +1,73 @@
+document.getElementById('error-msg').style.display = 'none'
+const searchFood = () => {
+    const searchField = document.getElementById('search-field')
+    const searchFieldText = searchField.value
+    // clear data 
+    searchField.value = ''
+    document.getElementById('error-msg').style.display = 'none'
+    if (searchFieldText == '') {
+        // please wright something to display
+    }
+    else {
+        // load data 
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFieldText}`
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+            .catch(error => displayError(error))
+    }
+
+}
+const displayError = error => {
+    document.getElementById('error-msg').style.display = 'block'
+}
+
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result')
+    searchResult.textContent = ''
+    if (meals.length == 0) {
+        //show no result found
+    }
+    meals.forEach(meal => {
+
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+       <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+       
+       <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+       <div class="card-body">
+       <h5 class="card-title">${meal.strMeal}</h5>
+       <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+       </div>
+       </div>
+           
+        `
+        searchResult.appendChild(div)
+    })
+}
+
+const loadMealDetail = mealId => {
+    console.log(mealId)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = meal => {
+    console.log(meal)
+    const mealDetails = document.getElementById('meal-details')
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    `
+    mealDetails.appendChild(div)
+}
